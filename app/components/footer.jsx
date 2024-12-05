a25b3dd0d68588f88748344e21358f9ff7f6d9b0
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-12-05 22:43:40 +0530

    main

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/mayank-ghatawal/" className="text-[#16f2b3]">Mayank Ghatawal</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/mayank-ghatawal/" className="text-[#16f2b3]">Mayank Ghatawal</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
