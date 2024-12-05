1146737c47f093df25cea4d2bd1a6ef7bed0c00a
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-12-05 22:56:00 +0530

    main

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -9,7 +9,9 @@ import { MdDownload } from "react-icons/md";
 import { RiContactsFill } from "react-icons/ri";
 import { SiLeetcode } from "react-icons/si";
 import hero from "../../../../public/hero.svg";
-import Typewriter from "typewriter-effect";
+import dynamic from "next/dynamic";
+
+const Typewriter = dynamic(() => import("typewriter-effect"), { ssr: false });
 
 function HeroSection() {
   return (
@@ -24,12 +26,11 @@ function HeroSection() {
 
       <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
-          {/* <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
+          <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
             Hello, <br />
             This is <span className=" text-pink-500 flex-col">Mayank Ghatawal</span>
-            {` , I'm a Professional `}
+            {` , I'm a`}
             <span className=" text-[#16f2b3]">
-              Full Stack Developer
               <Typewriter
                 options={{
                   strings: ["Full Stack Developer.", "UI/UX Designer.", "A.I. Developer."],
@@ -38,27 +39,6 @@ function HeroSection() {
                   delay: 75,
                 }}
               />
-            </span>
-          </h1> */}
-          <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
-            Hello, <br />
-            This is <span className="text-pink-500">Mayank Ghatawal</span>,{" "}
-            <span className="inline">
-              I'm a{" "}
-              <span className="text-[#16f2b3]">
-                <Typewriter
-                  options={{
-                    strings: [
-                      "Full Stack Developer .",
-                      "UI/UX Designer .",
-                      "A.I. Developer .",
-                    ],
-                    autoStart: true,
-                    loop: true,
-                    delay: 75,
-                  }}
-                />
-              </span>
             </span>
           </h1>
 
